b8f21af483dac960b1f54a2ffce16af851b4ba5d
kaikanwu <wukaikan@gmail.com>
2019-05-22 17:55:21 +0800

    update mybatis content

---
+++ src/main/java/com/kk/learn/ssm/chapter3/mapper/RoleMapper.java
@@ -0,0 +1,11 @@
+package com.kk.learn.ssm.chapter3.mapper;
+
+import com.kk.learn.ssm.chapter3.pojo.Role;
+
+/**
+ * @author kaikanwu
+ * @date 5/22/2019
+ */
+public interface RoleMapper {
+    public Role getRole(Long id);
+}
---
+++ src/main/java/com/kk/learn/ssm/chapter3/pojo/Role.java
@@ -0,0 +1,14 @@
+package com.kk.learn.ssm.chapter3.pojo;
+
+import lombok.Data;
+
+/**
+ * @author kaikanwu
+ * @date 5/22/2019
+ */
+@Data
+public class Role {
+    private Long id;
+    private String roleName;
+    private String note;
+}
